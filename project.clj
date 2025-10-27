f67b0432a371261fa6f031b55a557a986f96fdf6
MagusMachinae <Edward.Hughes1911@gmail.com>
2025-10-27 16:16:55 +0000

    sort out deps

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
 (defproject todomvcc "0.1.0"
-  :dependencies [[org.clojure/clojure "1.12.0"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [com.xtdb/xtdb-api "2.0.0"]
-                 [org.postgresql/postgresql "42.7.7"]
+                 [com.xtdb/xtdb-core "2.0.0"]
+                 [metosin/malli "0.16.4"]
                  [com.github.seancorfield/next.jdbc "1.3.1048"]]
+  :repositories [["sonatype-snapshots" {:url "https://s01.oss.sonatype.org/content/repositories/snapshots"}]]
   :source-paths ["src"]
   :resource-paths ["resources"]
-  :profiles {}
-  :aliases {}
-  :jvm-opts ["--add-opens=java.base/java.nio=ALL-UNNAMED"
-             "-Dio.netty.tryReflectionSetAccessible=true"]
+  :aliases {"xtdb" ["with-profile" "+xtdb" "run"]}
+  :profiles {:xtdb 
+             {:jvm-opts ["--add-opens=java.base/java.nio=ALL-UNNAMED"
+                         "-Dio.netty.tryReflectionSetAccessible=true"]}}
   :target-path "target/%s"
   :main todomvcc.shared.server)
